8176bbc402e0d2af8b9245a43c34f28f7bd467e8
Aaron Deril Dsouza <aaronddouza@gmail.com>
2023-08-13 23:00:35 +0530

    Start work on functionality

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var count = 0;
+var tasks = Object.keys(store.local.getAll(tasks)).length ? store.local.getAll(tasks) : {};
 
 //Specifying the location of static files and setting view engine to ejs
 app.use(express.static("public"));
@@ -19,6 +21,8 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('common'));
 
+
+//Serve landing page
 app.get("/", (req,res) => {
     try {
         res.render("index.ejs");
@@ -27,8 +31,27 @@ app.get("/", (req,res) => {
     }
 });
 
+// Basic Function to add tasks
 app.post("/addTask", (req,res) => {
-    res.send(req.body);
+    count++;    
+    tasks = store.local.set(`task${count}`,req.body);
+    res.render(`index.ejs`,tasks);
+});
+
+// Testing Function
+app.get("/getTask",(req,res)=> {
+    res.send(store.local.get(`task${count}`));
+    console.log((store.local.getAll(tasks))[`task${count}`].task);
+    console.log((store.local.getAll(tasks))[`task${count}`].priority);
+    console.log(Object.keys(store.local.getAll(tasks)).length);
+    console.log(store.local.getAll(tasks));
+});
+
+// Basic Function to remove tasks
+app.post("/removeTask",(req,res) => {
+    store.local.remove(`task${count}`);
+    count--;
+    res.render("index.ejs");
 });
 
 app.listen (port, () => {
