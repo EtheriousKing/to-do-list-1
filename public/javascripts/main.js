8f96d5a4ac8bed9b5a9e5cb0b98fabadf992baa3
Aaron Deril Dsouza <aaronddouza@gmail.com>
2023-08-17 11:52:42 +0530

    completed daily Tasks functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,76 +20,75 @@ function toggleButton (event) {
     }
 }
 
+$( window ).on("load", function() {
+    loadTasks();
+});
+
 $("#dailyForm").on("submit", function (e) {
     e.preventDefault();
     addTask();
 });
 
-    // On app load, get all tasks from localStorage
-    window.onload = loadTasks;
-
-    function loadTasks() {
-      let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
-      tasks.forEach(task => {
+function loadTasks() {
+    if (localStorage.getItem("dailyTaskList") == null) return;
+    var tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
+    if(tasks.length > 0) {
+        tasks.forEach(task => {
         const list = $("#dailyPendingTasks");
-        list.append(`<li><input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
-        <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+        const li = $("<li></li>");
+        list.append(
+            li.html(
+                `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}/>
+                <input type="text" value="${task.task}" class="${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+                <button class="del" onclick="removeTask(this)">Delete</button>`
+            ));
+        });
+    }
+}
+
+function addTask() {
+    const list = $("#dailyPendingTasks");
+    localStorage.setItem("dailyTaskList", JSON.stringify([...JSON.parse(localStorage.getItem("dailyTaskList") || "[]"), { task: dailyTask.val(), completed: false }]));
+    list.prepend(
+        `<li><input type="checkbox" onclick="taskComplete(this)"} class="check" ${task.completed ? 'checked' : ''}>
+        <input type="text" value="${dailyTask.val()}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
         <button class="del" onclick="removeTask(this)">Delete</button></li>`);
-      });
-    }
+    dailyTask.val("");
+}
 
-    function addTask() {
-      const list = $("#dailyPendingTasks");
-      localStorage.setItem("dailyTaskList", JSON.stringify([...JSON.parse(localStorage.getItem("dailyTaskList") || "[]"), { task: dailyTask.val(), completed: false }]));
-      list.append(`<li><input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
-      <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-      <button class="del" onclick="removeTask(this)">Delete</button></li>`);
-      dailyTask.val("");
+function taskComplete(event) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
+    tasks.forEach(task => {
+    if (task.task === $(event).next().val()) {
+        task.completed = !task.completed;
     }
-
-    function taskComplete(event) {
-      let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
-      tasks.forEach(task => {
-        if (task.task === $(event).next().val()) {
-          task.completed = !task.completed;
-        }
-      });
-      localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
-      $(event).next().prop('disabled', function(index, prop){
-        return prop == true ? null : true;
     });
+    localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
+    $(event).next().toggleClass("completed");
+}
+
+function removeTask(event) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
+    tasks.forEach((task,index) => {
+    if (task.task === $(event).parent().find("input[type='text']").val()) {
+        tasks.splice(index, 1);
     }
+    });
+    localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
+    $(event).parent().remove();
+}
+var currentTask = null;
 
-    function removeTask(event) {
-      let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
-      tasks.forEach((task,index) => {
-        if (task.task === $(event).parent().next().find("input[type='text']").val()) {
-          tasks.splice(index, 1);
-        }
-      });
-      localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
-      $(event).parent().remove();
+function getCurrentTask(event) {
+    currentTask = $(event).val();
+}
+
+function editTask(event) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
+    tasks.forEach(task => {
+    if (task.task === currentTask) {
+        task.task = $(event).val();
     }
-
-    // store current task to track changes
-    var currentTask = null;
-
-    // get current task
-    function getCurrentTask(event) {
-      currentTask = $(event).val();
-    }
-
-    // edit the task and update local storage
-    function editTask(event) {
-      let tasks = Array.from(JSON.parse(localStorage.getItem("dailyTaskList")));
-      // check if task is empty
-
-      // update task
-      tasks.forEach(task => {
-        if (task.task === currentTask) {
-          task.task = $(event).val();
-        }
-      });
-      // update local storage
-      localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
-    }
+    });
+    localStorage.setItem("dailyTaskList", JSON.stringify(tasks));
+}
